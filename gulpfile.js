424103e19531229efdba23561a18fa643fd1ef41
Rafael Fagundes <rafael@iMac-de-Rafael.local>
2018-03-13 15:38:31 -0300

    Basic structure

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+const runSequence = require('run-sequence');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
+/* Clean dist folder */
+gulp.task('clean' , () => {
+    return del([
+        'dist/**/*', 
+    ]);
+});
+
+/* Static task */
+gulp.task('static', () =>
+    gulp.src('src/static/**/*.*')
+    .pipe(gulp.dest('./dist/static/'))
+);
+
+/* Styles task */
+gulp.task('styles', () =>
+    gulp.src('src/styles/*.sass')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'))
+);
+
+/* Pug task */
+gulp.task('pug', () =>
+    gulp.src('src/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./dist/'))
+);
+
+/* Scripts task */
+gulp.task('scripts', () =>
+    gulp.src('src/scripts/*.js')
+    .pipe(babel({
+        presets: ['env']
+    }))
+    .pipe(gulp.dest('./dist/scripts/'))
+);
+
+/* Build task */
+gulp.task('build' , (done) => {
+    runSequence('clean', 'styles', 'scripts', 'pug', 'static', done)
+});
+
+
+/* Serve task*/
+gulp.task('serve', ['build'] , () => {
+    
+    browserSync.init({
+        server: "./dist"
+    });
+
+    // Watch for styles modifications
+    gulp.watch('src/styles/*.*', ['styles']);
+
+    // Watch for scripts modifications
+    gulp.watch('src/scripts/*.*', ['scripts']);
+
+    // Watch for markup modifications
+    gulp.watch('src/*.pug', ['pug']);
+
+    // Reload browser on changes
+    gulp.watch("dist/**/*.*").on('change', browserSync.reload);
+
+});
+
+
+/* Default Task */
+gulp.task('default', ['serve']);
