3f68fdd11629e7e2ea5b660efc72b2cd119135ff
Rafael Fagundes <rafael@iMac-de-Rafael.local>
2018-03-14 20:01:03 -0300

    Beta version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,64 +1,101 @@
 class Calculator{
     constructor() {
-        this.numberX = 0;
-        this.numbery = 0;
-        this.operation = '';
+        this.powerState = false;
+
+        const powerButton = document.querySelector('.power-button');
+        powerButton.addEventListener('click', this.powerSwitch);
+
+        const clearButton = document.querySelector('.clear-button');
+        clearButton.addEventListener('click', (e) => {
+            this.clearDisplay(e);
+            e.stopPropagation();
+        });
+
+        const allNumPadButtons = document.querySelectorAll('.button');
+        allNumPadButtons.forEach(element => {
+            element.addEventListener('click', (e) => {
+                this.numPadHandler(e);
+                e.stopPropagation();
+            });
+        });
     }
 
-    add(){}
-    subtract(){}
-    divide(){}
-    multiply(){}
-    result(){}
-}
-
-function buttonListener(e){
-    switch(e.target.value) {
-        case "+":
-            calculator.add();
-            break;
-        case "-":
-            calculator.subtract();
-            break;
-        case "÷":
-            calculator.divide();
-            break;
-        case "×":
-            calculator.multiply();
-            break;
-        case "=":
-            calculator.result();
-            break;
-        default:
-            console.log(e.target.value);
+    result(){
+        const currentValue = document.querySelector('.digits');
+        try {
+            currentValue.innerHTML = eval(currentValue.innerHTML);
+        } catch (error) {
+            currentValue.innerHTML = "Error";
+        }
+        calculator.adaptDisplay();        
     }
-}
 
-function numPadInit(){
-    const allNumPadButtons = document.querySelectorAll('.button');
-    allNumPadButtons.forEach(element => {
-        element.addEventListener('click', buttonListener);
-    });
-}
+    adaptDisplay(){
+        const currentValue = document.querySelector('.digits');
+        const digitsStyle = document.getElementsByClassName('digits')[0].style;
+         if(currentValue.innerHTML.length > 46){
+            currentValue.innerHTML = " Take it easy, man! ";
+            calculator.adaptDisplay();
+        }
+        else if(currentValue.innerHTML.length > 36 && currentValue.innerHTML.length <= 46){
+            digitsStyle.fontSize  = "0.4em";
+        }
+        else if(currentValue.innerHTML.length > 18 && currentValue.innerHTML.length <= 36){
+            digitsStyle.fontSize  = "0.5em";
+        }
+        else if(currentValue.innerHTML.length > 12 && currentValue.innerHTML.length <= 18){
+            digitsStyle.fontSize  = "1.0em";
+        }
+        else{
+            digitsStyle.fontSize  = "1.5em";
+        }
+    }
 
-function powerButtonInit(){
-    const powerButton = document.querySelector('.power-button');
-    powerButton.addEventListener('click', (e) => {
+    addToDisplay(value){
+        const currentValue = document.querySelector('.digits');
+        
+        if (currentValue.innerHTML === "0"){
+            currentValue.innerHTML = value;
+        }
+        else{
+            currentValue.innerHTML += value;
+        }
+        calculator.adaptDisplay();
+    }
+
+    clearDisplay(){
+        const currentValue = document.querySelector('.digits');
+        currentValue.innerHTML = "0";
+        calculator.adaptDisplay();
+    }
+
+    powerSwitch(){
         const digits = document.getElementsByClassName('digits');
         if(digits[0].style.visibility === "hidden"){
+            calculator.clearDisplay();
             digits[0].style.visibility = "visible";
+            calculator.powerState = true;
         }
         else{
             digits[0].style.visibility = "hidden";
+            calculator.powerState = false;
         }
-    })
+    }
+
+    numPadHandler(e){
+        if (calculator.powerState){ 
+            switch(e.target.value) {
+                case "=":
+                    calculator.result();
+                    e.stopPropagation();
+                    break;
+                default:
+                    calculator.addToDisplay(e.target.value)
+                    e.stopPropagation();
+                    break;
+            }
+        }       
+    }
 }
 
-/* --- MAIN --- */
-function main(){
-    powerButtonInit();
-    numPadInit();
-}
-
-let calculator = new Calculator();
-main();
+const calculator = new Calculator();
