f1ab80654e3cbcb7c2f4b63aea423bd8d5d84ea4
Rafael Fagundes <rafael@iMac-de-Rafael.local>
2018-03-19 12:41:40 -0300

    Keyboard support

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -99,3 +99,88 @@ class Calculator{
 }
 
 const calculator = new Calculator();
+
+// Key binding
+document.body.addEventListener('keypress', function(e) {
+    switch(e.keyCode) {
+        case 111: // o (on / off)
+            calculator.powerSwitch();
+            e.stopPropagation();
+            break;
+        case 99: // c (clear)
+            calculator.clearDisplay();
+            e.stopPropagation();
+            break;
+        case 13: // enter (result)
+            calculator.result();
+            e.stopPropagation();
+            break;
+        case 61: // = (result)
+            calculator.result();
+            e.stopPropagation();
+            break;
+        case 43: // + (add)
+            calculator.addToDisplay('+')
+            e.stopPropagation();
+            break;
+        case 45: // - (subtract)
+            calculator.addToDisplay('-')
+            e.stopPropagation();
+            break;
+        case 42: // * (multiply)
+            calculator.addToDisplay('*')
+            e.stopPropagation();
+            break;
+        case 47: // / (divide)
+            calculator.addToDisplay('/')
+            e.stopPropagation();
+            break;
+        case 46: // . (dot)
+            calculator.addToDisplay('.')
+            e.stopPropagation();
+            break;
+        case 48: // 0
+            calculator.addToDisplay(0)
+            e.stopPropagation();
+            break;
+        case 49: // 1
+            calculator.addToDisplay(1)
+            e.stopPropagation();
+            break;
+        case 50: // 2
+            calculator.addToDisplay(2)
+            e.stopPropagation();
+            break;
+        case 51: // 3
+            calculator.addToDisplay(3)
+            e.stopPropagation();
+            break;
+        case 52: // 4
+            calculator.addToDisplay(4)
+            e.stopPropagation();
+            break;
+        case 53: // 5
+            calculator.addToDisplay(5)
+            e.stopPropagation();
+            break;
+        case 54: // 6
+            calculator.addToDisplay(6)
+            e.stopPropagation();
+            break;
+        case 55: // 7
+            calculator.addToDisplay(7)
+            e.stopPropagation();
+            break;
+        case 56: // 8
+            calculator.addToDisplay(8)
+            e.stopPropagation();
+            break;
+        case 57: // 9
+            calculator.addToDisplay(9)
+            e.stopPropagation();
+            break;
+        default:
+            e.stopPropagation();
+            break;
+    }
+});
